104fa11ae8ae7dd7cc4ec2f22f1a7981196b64c5
Maratib <maratib@gmail.com>
2021-02-26 23:41:11 +0500

    initial commit

--- docs/main.js
+++ docs/main.js
@@ -74,7 +74,8 @@ __webpack_require__.r(__webpack_exports__);
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 const environment = {
-    production: false
+    production: false,
+    appRoot: "/"
 };
 /*
  * For easier debugging in development mode, you can import the following file
